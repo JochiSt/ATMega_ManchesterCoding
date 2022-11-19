8b02c92bb2bd1bac116cecf3bfcf9d071c0299c4
JochiSt <jochen.steinmann@web.de>
2022-11-19 14:01:22 +0100

    added file with general configuration

---
+++ firmware/config.h
@@ -0,0 +1,11 @@
+/**
+ * Store some configuration values
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BAUDRATE    9600    ///< used baudrate to communicate with the world
+
+
+#endif /* CONFIG_H */
